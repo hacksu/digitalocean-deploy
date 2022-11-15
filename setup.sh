5c065585b972bfcca0e4a8369b20da6f9817cd70
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-11-15 18:48:58 -0500

    eeey

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 ## fetch package registry
-#sudo apt update -y
+sudo apt update -y
 ## install node & npm
-#sudo apt install -y nodejs npm
+sudo apt install -y nodejs npm
+npm install --global n
+n stable
 ## [optional] install yarn (alternative to npm)
-#npm install --global yarn
+npm install --global yarn
 
 # get node version
 echo "node $(node -v)"
