a9b05fc9ecd5875bf52be2faac4e0bce6481a0df
seitz <cseitz.dev@gmail.com>
2022-11-15 19:34:10 -0500

    did stuff

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
+
 app.listen(80);
 
 
 app.get('/', (req, res) => {
-    const now = new Date().toLocaleString();
-    console.log('someone visited our page at', now);
-    res.send(`Hello there! The time is ${now}!`)
+    res.send("Hello there! The time is " + new Date().toLocaleTimeString());
 })
+
+
+
