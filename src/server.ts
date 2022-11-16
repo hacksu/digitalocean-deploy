9eb33b212fb723f61811ccc2cb301d52a8fd88fd
seitz <cseitz.dev@gmail.com>
2022-11-15 19:38:39 -0500

    added static serve

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,13 @@ const app = express();
 app.listen(80);
 
 
-app.get('/', (req, res) => {
-    res.send("Hello there! The time is " + new Date().toLocaleTimeString());
-})
+// app.get('/', (req, res) => {
+//     res.send("Hello there! The time is " + new Date().toLocaleTimeString());
+// })
+
+app.use(express.static(__dirname + "/../../hacksu-2021/dist", {
+    index: ['index.html']
+}))
 
 
 
